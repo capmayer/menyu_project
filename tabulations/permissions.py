4a1a721e1c2917f6b684c2e978dd908f7b8c684b
Mayer <henriqmayer@gmail.com>
2016-10-24 22:59:58 -0200

    fixed bug at permissions

--- tabulations/permissions.py
+++ tabulations/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class TabulationPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if not request.user.is_authenticated():
-            if view.action == 'create':
+            if request.method == 'POST':
                 return True
             else:
                 return False
