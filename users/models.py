2c36e84c8e76e21581dd198703d824efff5577a6
capmayer <henriqmayer@gmail.com>
2017-01-11 02:59:50 -0200

    changed model

--- users/models.py
+++ users/models.py
@@ -6,14 +6,11 @@ class Establishment(models.Model):
     name = models.CharField(max_length=150)
     type_establishment = models.CharField(max_length=150)
     cnpj = models.CharField(max_length=20)
-    created = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
     address = models.CharField(max_length=150)
     phone = models.CharField(max_length=20)
     terms = models.BooleanField(default=False)
 
 class Client(models.Model):
     user = models.OneToOne(User, on_delete=models.CASCADE)
-    address = models.CharField(max_length=150, default="")
     phone = models.CharField(max_length=20, null=True)
     terms = models.BooleanField(default=True)
